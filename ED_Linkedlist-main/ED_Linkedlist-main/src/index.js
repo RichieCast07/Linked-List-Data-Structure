01bcd47465d83abb6512112b92287813fdb9ffe7
Sploit07 <150275003+RichieCast07@users.noreply.github.com>
2024-06-07 08:59:51 -0600

    Add files via upload

---
+++ ED_Linkedlist-main/ED_Linkedlist-main/src/index.js
@@ -0,0 +1,10 @@
+import { list } from "./dependecies.mjs";
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.push("Hola")
+list.push("Hola2")
+
+for (let i=0;i<list.size();i++)
+    console.log(list.getElementAt(i).getData());
